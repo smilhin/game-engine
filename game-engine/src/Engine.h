5287428e812bb3f580a22fa7bbf80309f63bcfa3
Smilhin Danyil <smilgin64.25@gmail.com>
2024-11-03 17:29:45 +0100

    Setup Basic App and entry point

---
+++ game-engine/src/Engine.h
@@ -0,0 +1,9 @@
+#pragma once
+
+//FOR GAMES AND APPLICATIONS
+
+#include "Engine/Application.h"
+
+//ENTRY POINT-
+#include "Engine/EntryPoint.h"
+//------------
---
+++ game/src/GameApp.cpp
@@ -0,0 +1,22 @@
+#include <Engine.h>
+
+class GameApp : public Engine::Application
+{
+
+public:
+	GameApp()
+	{
+
+	}
+	~GameApp()
+	{
+
+	}
+
+};
+
+
+Engine::Application* Engine::CreateApplication()
+{
+	return new GameApp();
+}
