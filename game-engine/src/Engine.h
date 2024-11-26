1590366cfda6bfff1fbcf64038c151cc666db7b0
Smilhin Danyil <smilgin64.25@gmail.com>
2024-11-26 15:52:11 +0100

    spdlog added

--- game-engine/src/Engine.h
+++ game-engine/src/Engine.h
@@ -2,7 +2,9 @@
 
 //FOR GAMES AND APPLICATIONS
 
+#include "Engine/Log.h"
 #include "Engine/Application.h"
+
 
 //ENTRY POINT-
 #include "Engine/EntryPoint.h"
--- game-engine/src/Engine/EntryPoint.h
+++ game-engine/src/Engine/EntryPoint.h
@@ -1,5 +1,6 @@
 #pragma once
 
+
 #ifdef EG_PLATFORM_WINDOWS
 
 extern Engine::Application* Engine::CreateApplication();
@@ -8,6 +9,11 @@ int main(int argc, char** argv)
 {
 
 	auto app = Engine::CreateApplication(); 
+
+	Engine::Log::Init();
+	EG_CORE_WARN("Initialised Core Logger");
+	int a = 5;
+	EG_CLIENT_INFO("Initialised Client Logger, Var = {0}", a);
 
 	app->Run();
 	delete app;
